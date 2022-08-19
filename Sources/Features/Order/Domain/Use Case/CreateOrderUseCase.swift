0a02dd123f5123ae6e1b5d1ec95147bee709cb92
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2022-08-19 11:53:38 -0300

    [FEATURE] - Reestruturação do projeto

---
+++ Sources/Features/Order/Domain/Use Case/CreateOrderUseCase.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rodrigo Souza on 19/08/22.
+//
+
+import Foundation
+
+func createOrderUseCase() {
+    
+}
---
+++ Sources/Features/Order/Domain/Use Case/ListOrderUseCase.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rodrigo Souza on 19/08/22.
+//
+
+import Foundation
+
+func listOrderUseCase() {}
